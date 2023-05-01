d977502c5ad69f6b84ef190f80bfee408b74b957
Marguerite Oneto <marguerite@oneto.us>
2023-05-01 17:56:07 +0000

    MO 01_11 Answer

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,21 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * FROM Customers
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater';
+-- CustomerID = 42
+
+SELECT * FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad';
+-- DishID = 9
+
+UPDATE Customers
+SET FavoriteDish = 9
+WHERE CustomerID = 42;
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name
+FROM Customers
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID
+WHERE Customers.CustomerID = 42;
+
