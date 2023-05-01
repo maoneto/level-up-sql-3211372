9ced1cedaad7e6b99fbd3f2c16639bd4d79893f4
Marguerite Oneto <marguerite@oneto.us>
2023-05-01 18:15:58 +0000

    MO 01_12 Answer

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,10 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT Customers.FirstName, Customers.LastName, Customers.Email, COUNT(Orders.OrderID) as OrderCount
+  FROM Orders
+  JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+  GROUP BY Orders.CustomerID
+  ORDER BY OrderCount DESC
+  LIMIT 15;
